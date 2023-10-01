8bec413b3384c0f903bc5ff17ee55a75289829b4
abircu <mdabircse4873@gmail.com>
2023-10-02 04:02:17 +0600

    firabase reaxt and context combo aplication

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className=" text-2xl font-600">
+      <h1>this is home page here</h1>
+    </div>
+  );
+};
+
+export default Home;
